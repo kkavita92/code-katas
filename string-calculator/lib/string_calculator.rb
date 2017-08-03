a734b045ee17d37affbfc76b030e46ba9ec0540a
kkavita92 <kkavita92@gmail.com>
2017-08-03 14:51:13 +0100

    Implement calculation for string of 1 number

--- string-calculator/lib/string_calculator.rb
+++ string-calculator/lib/string_calculator.rb
@@ -2,5 +2,6 @@ class StringCalculator
 
   def add(string_of_numbers)
     return 0 if string_of_numbers.length == 0
+    string_of_numbers.split(',').map(&:to_i).reduce(:+)
   end
 end
--- string-calculator/spec/string_calculator_spec.rb
+++ string-calculator/spec/string_calculator_spec.rb
@@ -3,7 +3,12 @@ require 'string_calculator'
 
 
 describe StringCalculator do
+
   it 'does calculations for empty string' do
     expect(subject.add('')).to eq(0)
+  end
+
+  it 'does calculation for string of 1 number' do
+    expect(subject.add('1')).to eq(1)
   end
 end
