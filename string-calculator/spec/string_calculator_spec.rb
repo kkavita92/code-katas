dd7fda390e9162408e1fecc0ad0fc5da71b318fd
kkavita92 <kkavita92@gmail.com>
2017-08-03 14:56:03 +0100

    Implements calculation for strings of unknown length

--- string-calculator/spec/string_calculator_spec.rb
+++ string-calculator/spec/string_calculator_spec.rb
@@ -8,11 +8,20 @@ describe StringCalculator do
     expect(subject.add('')).to eq(0)
   end
 
-  it 'does calculation for string of 1 number' do
-    expect(subject.add('1')).to eq(1)
+  describe 'does calculation for strings of any number' do
+    it 'does calculation for string of 1 number' do
+      expect(subject.add('1')).to eq(1)
+    end
+
+    it 'does calculation for string of 2 numbers' do
+      expect(subject.add('3,6')).to eq(9)
+    end
+
+    it 'does calculation for string of 10 numbers' do
+      expect(subject.add('4,2,1,2,5,6,10,3,6,9')).to eq(48)
+    end
   end
 
-  it 'does calculation for string of 2 numbers' do
-    expect(subject.add('3,6')).to eq(9)
-  end
+
+
 end
