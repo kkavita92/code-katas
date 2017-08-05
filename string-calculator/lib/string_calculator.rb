ea3f6a8157ed4ceffd0be5ee3d03ee8ecda7ad3d
kkavita92 <kkavita92@gmail.com>
2017-08-05 14:23:28 +0100

    Do some refactoring - incomplete

--- string-calculator/lib/string_calculator.rb
+++ string-calculator/lib/string_calculator.rb
@@ -4,4 +4,15 @@ class StringCalculator
     return 0 if string_of_numbers.length == 0
     string_of_numbers.split(',').map(&:to_i).reduce(:+)
   end
+
+  private
+
+  def split_string(string_of_numbers)
+    return string_of_numbers.split(',')
+  end
+
+  def convert_to_number(string_array)
+    return string_array.map(&:to_i)
+  end
+
 end
